44fde98242ac1ce2ffd4084197cf2cea8195f3f9
Ankit Srivastava <36195898+ankitcs03@users.noreply.github.com>
2018-05-09 02:38:33 +0530

    Delete form.py

--- PriceAlert/pricecheck/form.py
+++
@@ -1,7 +0,0 @@
-# from django import forms
-
-# class AlertForm(forms.Form):
-# 	source =  forms.CharField()
-# 	product_name = forms.CharField()
-# 	alert_price = forms.FloatField()
-# 	email_id =  forms.EmailField()
